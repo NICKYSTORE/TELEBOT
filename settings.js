209bcb89214c37db8ecb7984b10ae15f721ad4a3
NICKYSTORE <nickywanlin14@gmail.com>
2024-06-16 08:50:58 +0700

    telebot

---
+++ settings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 0887435047326
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': 'junaa', // 👉 login https://api.zeeoneofc.my.id to get apikey
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "6830062876:AAHs5_CFMs2FtGDRJJM6_rOd1UMK0hyZ5zA" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Alphabot-Mdོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ NICKY ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6282143965793" //your telegram number
+global.OWNER = ["https://t.me/@NICKYERROR", "https://t.me/@NICKYERROR"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
